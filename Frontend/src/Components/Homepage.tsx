1e413871a636d87f53443fad5518d0d0170b9d2a
techwithmuzzu <codewithmuzzu@gmail.com>
2024-09-25 10:53:00 +0530

    Created Components Homepage TemplatesPage.

--- Frontend/src/Components/Homepage.tsx
+++ Frontend/src/Components/Homepage.tsx
@@ -1,15 +1,7 @@
-import { Link, Route, Routes } from "react-router-dom";
-import TemplatesPage from "./TemplatesPage";
 
 function Homepage(){
     return <>
-    <h1>Home Page</h1>
-    <button>
-        <Link to="/templates">Create a Resume</Link>
-    </button>
-      <Routes>
-        <Route path='/templates' element={<TemplatesPage />}/>
-      </Routes>
+        <button>Create Resume</button>
     </>
 }
 
